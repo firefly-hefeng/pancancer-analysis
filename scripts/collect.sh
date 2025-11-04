f9360bfa7422a07db2181eebfde4c0368d3b6d10
PanCancer Research Team <researcher@pancancer.org>
2025-11-04 18:15:29 +0800

    chore: update metadata schema version

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -2,3 +2,4 @@
 # Commit 22: feat: add multi-sample processing script - 1775143686
 # Commit 35: feat: add logging mechanism - 1775143699
 # Commit 48: feat: implement Broad SCP collector - 1775143711
+# Commit 9: chore: update metadata schema version - 1775143741
